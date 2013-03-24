3892f1514a3bba3a85c54504759eaf123d29a204
yoshik <fujiwara.access@gmail.com>
2013-03-24 21:01:48 +0900

    add websocket (only publish)

---
+++ app/controllers/Synapse.scala
@@ -0,0 +1,67 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+import play.api.libs.iteratee._
+import play.api.libs.concurrent._
+import scala.collection.mutable.ListBuffer
+
+object Synapse extends Controller {
+
+  object Brain{
+    var listBuf = new ListBuffer[Synapse]
+    private var id_count:Long=0;
+    def makeId():Long={
+      synchronized{
+        id_count+=1;
+        if(id_count==Long.MaxValue){id_count=0;}
+        return id_count;
+      }
+    }
+  }
+
+  class Synapse (arg_tag:String)(arg_ws:PushEnumerator[String]) {
+
+    val tag = arg_tag
+    val ws = arg_ws
+    val id:Long = Brain.makeId()
+
+    //join
+    Brain.listBuf.append(this)
+
+    def notify ( tag : String )( msg : String ) = {
+      Brain.listBuf.foreach { i=>
+        if(i.tag == tag){
+          i.ws.push(msg)
+        }
+      }
+    }
+
+    def remove () = {
+      println("quit"+id)
+      for(i <- 0 to Brain.listBuf.length) {
+        if(Brain.listBuf(i).id==id){
+          Brain.listBuf.remove(i)
+          //TODO slow...
+        }
+      }
+    }
+
+  }
+
+
+  def ws(tag:String) = WebSocket.async[String] {
+    request => {
+      val out = Enumerator.imperative[String]()
+      val Synapse = new Synapse(tag)(out)
+      val in = Iteratee.foreach[String](text => {
+        Synapse.notify(tag)(text)
+      }).mapDone(_ => {
+        Synapse.remove()
+      })
+      Promise.pure(in -> out)
+    }
+  }
+
+}
